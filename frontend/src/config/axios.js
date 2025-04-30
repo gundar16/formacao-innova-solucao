71ebe097c02d4c631e9158c03cdd391b9eca280e
Simoes <pedrosimoes.ps@hotmail.com>
2025-04-30 15:50:45 +0100

    Small change for env

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_URL,
+  baseURL: import.meta.env.VITE_REACT_APP_URL || 'http://localhost:3002',
   headers: {'Allow-Control-Allow-Origin': '*'}
 });
 
